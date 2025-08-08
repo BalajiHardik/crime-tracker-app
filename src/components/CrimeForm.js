43cf5bc37ec34a920e239c030decfa94aca74bb8
BalajiHardik <thangabalaji.periya@relevantz.com>
2025-08-08 14:53:14 +0530

    final

--- src/components/CrimeForm.js
+++ src/components/CrimeForm.js
@@ -18,11 +18,32 @@ function CrimeForm({ onAdd }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder="Criminal Name" value={crime.CriminalName} onChange={e => setCrime({ ...crime, CriminalName: e.target.value })} />
-      <input placeholder="Details" value={crime.Details} onChange={e => setCrime({ ...crime, Details: e.target.value })} />
-      <input placeholder="Status" value={crime.CriminalStatus} onChange={e => setCrime({ ...crime, CriminalStatus: e.target.value })} />
-      <button type="submit">Add Crime</button>
-    </form>
+    <input
+      placeholder="Criminal Name"
+      value={crime.CriminalName}
+      onChange={e => setCrime({ ...crime, CriminalName: e.target.value })}
+      required
+    />
+    <input
+      placeholder="Details"
+      value={crime.Details}
+      onChange={e => setCrime({ ...crime, Details: e.target.value })}
+      required
+    />
+    <select
+      value={crime.CriminalStatus}
+      onChange={e => setCrime({ ...crime, CriminalStatus: e.target.value })}
+      required
+    >
+      <option value="">Select Status</option>
+      <option value="Under Investigation">Under Investigation</option>
+      <option value="Convicted">Convicted</option>
+      <option value="Released">Released</option>
+      <option value="Fugitive">Fugitive</option>
+    </select>
+    <button type="submit">Add Crime</button>
+  </form>
+  
   );
 }
 
